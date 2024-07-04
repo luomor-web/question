5cefcad171e5607170979c33a678c9ae3de931a2
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-07-04 14:32:22 +0800

    update

--- miniprogram/pages/confirm/index.js
+++ miniprogram/pages/confirm/index.js
@@ -1,15 +1,15 @@
 Page({
-    data:{
-        showAd:getApp().globalData.showAd,
+    data: {
+        showAd: getApp().globalData.showAd,
         selectCategory: '',
     },
-    onLoad( ){
+    onLoad() {
         //TODO 关于题库不足100题时答题提示的问题
         this.init();
     },
- 
-    init(){
-        let selectCategory = wx.getStorageSync('selectCategory') ;
+
+    init() {
+        let selectCategory = wx.getStorageSync('selectCategory');
         /*
         if (selectCategory.indexOf('>') > -1 ) {
             selectCategory=selectCategory.substring(selectCategory.indexOf('>')+1);
@@ -18,20 +18,20 @@ Page({
             selectCategory=selectCategory.substring(selectCategory.indexOf('>')+1);
         }
         */
-        this.setData({selectCategory:selectCategory})
+        this.setData({ selectCategory: selectCategory })
     },
-    startExam(){
-        let cid = wx.getStorageSync('cid') ;
-        this.go('question', 'cid='+cid+'&type=4');
+    startExam() {
+        let cid = wx.getStorageSync('cid');
+        this.go('question', 'cid=' + cid + '&type=4');
     },
-    go(url,params){
+    go(url, params) {
         wx.redirectTo({
-            url:'/pages/'+url+'/index?' + params 
+            url: '/pages/' + url + '/index?' + params
         });
     },
-    exportExam(){
-        this.go('exportq','type=1')
+    exportExam() {
+        this.go('exportq', 'type=1')
     }
- 
- 
+
+
 })
